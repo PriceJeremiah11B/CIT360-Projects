ecb194eee2178ff4b2692f4cf3ce886febc4ab06
JeremiahP11B <52722550+JeremiahP11B@users.noreply.github.com>
2019-09-24 22:10:17 -0600

    updated

--- JSON Tutorial/src/com/tcg/json/JSONUtils.java
+++ JSON Tutorial/src/com/tcg/json/JSONUtils.java
@@ -3,6 +3,8 @@ package com.tcg.json;
 import java.io.InputStream;
 import java.util.Scanner;
 
+import org.json.JSONObject;
+
 public class JSONUtils {
 	 public static String getJSONSStringFromFile(String path) {
 		 Scanner scanner;
@@ -13,4 +15,21 @@ public class JSONUtils {
 		 return json;
 		 
 	 }
+	 
+	 public static JSONObject getJSONObjectFromFile(String path) {
+		 return new JSONObject(getJSONObjectFromFile(path));
+	 }
+	 
+	 public static boolean objectExists(JSONObject jsonObject, String key) {
+		 Object o;
+		 try {
+			o = jsonObject.get(key);
+		 }
+		 catch(Exception e) {
+			 return false;
+		 }
+		 return o != null;
+		 
+	 }
+	 
 }
