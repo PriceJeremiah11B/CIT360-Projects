45f124367727caa02bb97a8a07585beec6013618
PriceJeremiah11B <52722550+PriceJeremiah11B@users.noreply.github.com>
2019-10-08 17:57:31 -0600

    77

--- Java Collections/src/Collections.java
+++ Java Collections/src/Collections.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.TreeMap;
 
 public class Collections {
@@ -6,6 +9,18 @@ public class Collections {
 			String[] fruit = { "Apples", "Pears", "Tomatoes", "Grapes"};
 			
 			System.out.println("Standard fruity array " + fruit + "\n");
+			
+			List<String> fruitList = new ArrayList<String>(Arrays.asList(fruit));
+			
+			System.out.println("This is a List: " + fruitList + "\n");
+			fruitList.add("Apples");
+			fruitList.add("Grapes");
+			//these are duplicate values, which the array will allow to add, regardless.
+			//the array is non-bias against the data.
+			
+			System.out.println("List after adding things: " + fruitList + "\n");
+			
+			
 		}
 	
 }
