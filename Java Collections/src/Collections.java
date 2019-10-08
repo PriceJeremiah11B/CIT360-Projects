94c2552c339cbf2fd2667510e60abc4b2c9d927e
PriceJeremiah11B <52722550+PriceJeremiah11B@users.noreply.github.com>
2019-10-08 17:48:28 -0600

    oi

--- Java Collections/src/Collections.java
+++ Java Collections/src/Collections.java
@@ -1,5 +1,11 @@
-import 
+import java.util.TreeMap;
 
 public class Collections {
 
+		public static void main(String[] args) {
+			String[] fruit = { "Apples", "Pears", "Tomatoes", "Grapes"};
+			
+			System.out.println("Standard fruity array " + fruit + "\n");
+		}
+	
 }
